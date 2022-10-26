e0e6cd6ea81ed34eddb553f62c231c2866707254
MarioMolina23 <94089210+MarioMolina23@users.noreply.github.com>
2022-10-25 19:27:55 -0600

    Mejoré la vista del login
    Arreglar el menú de la pantalla del login. Poner más elegante el "home" de la parte superior izquierda

--- View/generales.php
+++ View/generales.php
@@ -4,6 +4,7 @@
   {
     echo '
     	<!-- title icon -->
+        <title>Home</title>
         <link rel="shortcut icon" type="image/png" href="assets/img/icon.png">
         <!-- google font -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
@@ -57,10 +58,11 @@
   {
       echo '
         <title>Login</title>
+        <link rel="shortcut icon" type="image/png" href="assets/img/icon.png">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/login.css">';
+        <link rel="stylesheet" href="../assets/css/login.css">';
   }
